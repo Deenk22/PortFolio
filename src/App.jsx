3332ead89a560cb9ddbfe1cec4e4d4a1d14ba52c
Deenk22 <smj_personal@hotmail.com>
2023-11-09 11:37:11 +0100

    Tailwind config Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-center text-blue-400">
+        Hello world we ar working with tailwinds and react
+      </h1>
+    </>
+  );
+}
+
+export default App;
