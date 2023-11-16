fea3a25a81d48f3b8a7bcccba3e037fcbac14d86
Deenk22 <129082676+Deenk22@users.noreply.github.com>
2023-11-16 10:21:56 +0100

    New Design - Carrousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,14 +1,19 @@
-import {Swiper, SwiperSlide} from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
-import "swiper/css/effect-coverflow";
-import {Autoplay, EffectCoverflow, FreeMode, Pagination} from "swiper/modules";
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {projects} from "../../constants";
-import {RxArrowTopRight} from "react-icons/rx";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-coverflow';
+import {
+  Autoplay,
+  EffectCoverflow,
+  FreeMode,
+  Pagination,
+} from 'swiper/modules';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { projects } from '../../constants';
+import { RxArrowTopRight } from 'react-icons/rx';
 
 export default function Carrousel() {
   const [isActive, setIsActive] = useState(false);
@@ -18,22 +23,22 @@ export default function Carrousel() {
   };
 
   return (
-    <div className="grid place-content-center">
-      <div className="flex flex-col justify-center items-center mt-[12vh] mb-6">
+    <div className="flex flex-row justify-evenly items-center">
+      <div className="flex flex-col justify-center items-center">
         <h1 className="text-white-section1 text-5xl font-main">Projects</h1>
         <p className="text-white-section1 text-lg font-main">
-          Find Your{" "}
+          Find Your{' '}
           <span className="text-hover-font text-xl">Favorite Projects</span>
         </p>
         <button className="button" onClick={toggleChangeColor}>
-          {isActive ? "Black" : "White"}
+          {isActive ? 'Black' : 'White'}
         </button>
       </div>
       <Swiper
         modules={[FreeMode, Pagination, Autoplay, EffectCoverflow]}
         centeredSlides={true}
-        slidesPerView={"auto"}
-        effect={"coverflow"}
+        slidesPerView={'auto'}
+        effect={'coverflow'}
         grabCursor={true}
         coverflowEffect={{
           rotate: 18,
@@ -68,8 +73,8 @@ export default function Carrousel() {
           },
         }}
         freeMode={true}
-        autoplay={{delay: 3000}}
-        className="max-w-[100%] lg:max-w-[56%]"
+        autoplay={{ delay: 3000 }}
+        className="max-w-[100%] lg:max-w-[56%] h-screen py-32 mx-0"
       >
         {projects.map((item) => {
           return (
@@ -78,23 +83,23 @@ export default function Carrousel() {
                 <div
                   className={
                     isActive
-                      ? "absolute inset-0 bg-white-section1 opacity-70 rounded-xl transition-all duration-300"
-                      : "absolute inset-0 bg-black-deenk opacity-70 rounded-xl transition-all duration-300"
+                      ? 'absolute inset-0 bg-white-section1 opacity-70 rounded-xl transition-all duration-300'
+                      : 'absolute inset-0 bg-black-deenk opacity-70 rounded-xl transition-all duration-300'
                   }
                 />
                 <div className="relative flex flex-col gap-4">
                   <item.icon
                     className={
                       isActive
-                        ? "text-black-font w-[32px] h-[32px]"
-                        : "text-white-section1 w-[32px] h-[32px]"
+                        ? 'text-black-font w-[32px] h-[32px]'
+                        : 'text-white-section1 w-[32px] h-[32px]'
                     }
                   />
                   <h1
                     className={
                       isActive
-                        ? "text-lg md:text-3xl font-main text-black-font"
-                        : "text-lg md:text-3xl font-main text-white-section1"
+                        ? 'text-lg md:text-3xl font-main text-black-font'
+                        : 'text-lg md:text-3xl font-main text-white-section1'
                     }
                   >
                     <strong>{item.title}</strong>
@@ -102,8 +107,8 @@ export default function Carrousel() {
                   <p
                     className={
                       isActive
-                        ? "text-base md:text-lg font-main text-black-deenk max-w-[50%] sm:max-w-[60%] md:max-w-[70%] lg:max-w-[80%]"
-                        : "text-base md:text-lg font-main text-white-section1 opacity-50 max-w-[50%] sm:max-w-[60%] md:max-w-[70%] lg:max-w-[80%] xl:max-w-[100%]"
+                        ? 'text-base md:text-lg font-main text-black-deenk max-w-[50%] sm:max-w-[60%] md:max-w-[70%] lg:max-w-[80%]'
+                        : 'text-base md:text-lg font-main text-white-section1 opacity-50 max-w-[50%] sm:max-w-[60%] md:max-w-[70%] lg:max-w-[80%] xl:max-w-[100%]'
                     }
                   >
                     {item.content}
@@ -112,7 +117,7 @@ export default function Carrousel() {
                 <div>
                   <Link to={`projectdetails/${item.id}`}>
                     <RxArrowTopRight
-                      size={"2em"}
+                      size={'2em'}
                       className="absolute bottom-5 left-3 xl:left-6 text-white-section1 opacity-50"
                     />
                   </Link>
