999e69083970dbcf9bd9225a722b86ccbcfe07b8
Deenk22 <smj_personal@hotmail.com>
2024-01-03 08:32:49 +0100

    Deployment: Adjusting Language / Icons.

--- src/components/TranslationButton/TranslationButton.jsx
+++ src/components/TranslationButton/TranslationButton.jsx
@@ -4,15 +4,15 @@ import {CiGlobe} from "react-icons/ci";
 export default function TranslationButton() {
   const {i18n} = useTranslation();
 
-  const handleChangeLanguage = () => {
+  function handleLanguage() {
     i18n.language === "es"
       ? i18n.changeLanguage("en")
       : i18n.changeLanguage("es");
-  };
+  }
 
   return (
     <CiGlobe
-      onClick={handleChangeLanguage}
+      onClick={handleLanguage}
       className="relative bottom-6 right-[14px] md:bottom-0 md:right-0 text-[25px] text-white-section1 mt-[1.4px] cursor-pointer mr-2 duration-200 hover:text-hover-font"
     />
   );
