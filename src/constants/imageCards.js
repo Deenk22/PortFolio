89598fad3e52eac4765aacfbbf28ba69312c38ed
Deenk22 <smj_personal@hotmail.com>
2024-01-03 07:40:12 +0100

    Deployment: Adjusting Images (projects).

--- src/constants/imageCards.js
+++ src/constants/imageCards.js
@@ -1,28 +1,28 @@
 const imageCard = [
   {
     title: "Creatures",
-    src: "../src/assets/img/creatures.webp",
+    src: "/src/assets/img/creatures.webp",
     description:
       "Terrifying creature sound sample packs ready to add to your game!. listen to it with headphones.",
     link: "https://www.youtube.com/watch?v=bzlEqrVk_-c",
   },
   {
     title: "Robot Movements",
-    src: "../src/assets/img/robotMovements.webp",
+    src: "/src/assets/img/robotMovements.webp",
     description:
       "Some tips on how to create impressive robot moves for your video game. Go and download them, they're free!.",
     link: "https://www.youtube.com/watch?v=PgSndNKucps",
   },
   {
     title: "Arrival",
-    src: "../src/assets/img/arrival.webp",
+    src: "/src/assets/img/arrival.webp",
     description:
       "Abstract sounds produced by unknown technology. Beings from another planet.",
     link: "https://www.youtube.com/watch?v=F3fCwAjWFBg",
   },
   {
     title: "MenuUI Cartoon & Arcade",
-    src: "../src/assets/img/cartoon&arcade.webp",
+    src: "/src/assets/img/cartoon&arcade.webp",
     description:
       "MenuUI Cartoon & Arcade Vol 1. Fun and quality sounds ready to be dragged into your project.",
     link: "https://www.youtube.com/watch?v=6J25Mju6bGw&t=48s",
